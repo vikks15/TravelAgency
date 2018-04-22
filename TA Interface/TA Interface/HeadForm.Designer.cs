e09a7c3d947510b60617487f2ca7198748bed56c
vikks15 <lva9@mail.ru>
2018-04-22 14:42:44 +0300

    HeadForm tables page

--- TA Interface/TA Interface/HeadForm.Designer.cs
+++ TA Interface/TA Interface/HeadForm.Designer.cs
@@ -28,11 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ReportsPage = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.TourOpGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TablePage = new System.Windows.Forms.TabPage();
+            this.GridViewPage1 = new System.Windows.Forms.DataGridView();
+            this.TableNameLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TableComboBox = new System.Windows.Forms.ComboBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.Menu = new System.Windows.Forms.TabControl();
+            this.ReportsPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TourOpGridView)).BeginInit();
+            this.TablePage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewPage1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ReportsPage
+            // 
+            this.ReportsPage.Controls.Add(this.button1);
+            this.ReportsPage.Controls.Add(this.label1);
+            this.ReportsPage.Controls.Add(this.TourOpGridView);
+            this.ReportsPage.Controls.Add(this.button2);
+            this.ReportsPage.Location = new System.Drawing.Point(4, 22);
+            this.ReportsPage.Name = "ReportsPage";
+            this.ReportsPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ReportsPage.Size = new System.Drawing.Size(853, 452);
+            this.ReportsPage.TabIndex = 1;
+            this.ReportsPage.Text = "Отчеты";
+            this.ReportsPage.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(670, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 36);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Удалить запись";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // TourOpGridView
+            // 
+            this.TourOpGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TourOpGridView.Location = new System.Drawing.Point(34, 54);
+            this.TourOpGridView.Name = "TourOpGridView";
+            this.TourOpGridView.Size = new System.Drawing.Size(486, 173);
+            this.TourOpGridView.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(657, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 36);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Добавить запись";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TablePage
+            // 
+            this.TablePage.Controls.Add(this.QuitButton);
+            this.TablePage.Controls.Add(this.panel1);
+            this.TablePage.Controls.Add(this.TableNameLabel);
+            this.TablePage.Controls.Add(this.GridViewPage1);
+            this.TablePage.Location = new System.Drawing.Point(4, 22);
+            this.TablePage.Name = "TablePage";
+            this.TablePage.Padding = new System.Windows.Forms.Padding(3);
+            this.TablePage.Size = new System.Drawing.Size(853, 452);
+            this.TablePage.TabIndex = 0;
+            this.TablePage.Text = "Таблицы";
+            this.TablePage.UseVisualStyleBackColor = true;
+            // 
+            // GridViewPage1
+            // 
+            this.GridViewPage1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.GridViewPage1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.GridViewPage1.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.GridViewPage1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridViewPage1.Location = new System.Drawing.Point(24, 32);
+            this.GridViewPage1.Name = "GridViewPage1";
+            this.GridViewPage1.Size = new System.Drawing.Size(555, 368);
+            this.GridViewPage1.TabIndex = 7;
+            // 
+            // TableNameLabel
+            // 
+            this.TableNameLabel.AutoSize = true;
+            this.TableNameLabel.Location = new System.Drawing.Point(24, 10);
+            this.TableNameLabel.Name = "TableNameLabel";
+            this.TableNameLabel.Size = new System.Drawing.Size(103, 13);
+            this.TableNameLabel.TabIndex = 9;
+            this.TableNameLabel.Text = "Название таблицы";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.ShowButton);
+            this.panel1.Controls.Add(this.SaveButton);
+            this.panel1.Controls.Add(this.TableComboBox);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(594, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 163);
+            this.panel1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Таблица";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // TableComboBox
+            // 
+            this.TableComboBox.FormattingEnabled = true;
+            this.TableComboBox.Location = new System.Drawing.Point(76, 6);
+            this.TableComboBox.Name = "TableComboBox";
+            this.TableComboBox.Size = new System.Drawing.Size(133, 21);
+            this.TableComboBox.TabIndex = 8;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(154, 124);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 9;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(64, 124);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 13;
+            this.ShowButton.Text = "Показать";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Location = new System.Drawing.Point(24, 421);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(75, 23);
+            this.QuitButton.TabIndex = 10;
+            this.QuitButton.Text = "Выйти";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.Menu.Controls.Add(this.TablePage);
+            this.Menu.Controls.Add(this.ReportsPage);
+            this.Menu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.SelectedIndex = 0;
+            this.Menu.Size = new System.Drawing.Size(861, 478);
+            this.Menu.TabIndex = 8;
+            // 
+            // HeadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "HeadForm";
+            this.ClientSize = new System.Drawing.Size(861, 478);
+            this.Controls.Add(this.Menu);
+            this.Name = "HeadForm";
+            this.Text = "АИС Турагентство: Руководитель";
+            this.ReportsPage.ResumeLayout(false);
+            this.ReportsPage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TourOpGridView)).EndInit();
+            this.TablePage.ResumeLayout(false);
+            this.TablePage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewPage1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.Menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabPage ReportsPage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView TourOpGridView;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage TablePage;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.ComboBox TableComboBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label TableNameLabel;
+        private System.Windows.Forms.DataGridView GridViewPage1;
+        private System.Windows.Forms.TabControl Menu;
+
     }
 }
--- TA Interface/TA Interface/HeadForm.cs
+++ TA Interface/TA Interface/HeadForm.cs
@@ -7,14 +7,135 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TA_Interface
 {
     public partial class HeadForm : Form
     {
+        SqlConnection newcon;
+        SqlDataAdapter adap;
+        DataSet data;
+        string[] headerNames = new string[10];
+
         public HeadForm()
         {
             InitializeComponent();
+            string[] tables = { "Туры", "Проживаниие", "Авиакомпании", "Страхование" };
+            TableComboBox.Items.AddRange(tables);
+        }
+
+        private void Menu_SelectedIndexChanged()
+        {
+            MessageBox.Show(this, this.Menu.SelectedTab.Text.Trim());
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            //GridViewPage1.Rows.Clear(); - вариант 1
+            GridViewPage1.ColumnHeadersVisible = true;
+            string query = "", tableName = "";
+            int numOfColumns = 0;
+
+            string way = "Data Source=VICKY-PC\\SQLEXPRESS;Initial Catalog=TravelAgency;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(way);
+            conn.Open();
+
+            switch (TableComboBox.Text)
+            {
+                case "Туры":
+                    {                        
+                        TableNameLabel.Text = "Туры";
+                        query = @"SELECT IdTour, Country, City, BeginDate, EndDate, Price, TourOpName, AirlineName, AcName, CompanyName
+                                    FROM Tour, TourOperator, Airline, Accommodation, Insurance
+                                    WHERE (TourOpId = IdTourOperator) AND (AirlineId = IdAirline) AND (AccommodationId = IdAccommodation) AND (InsuranceId = IdInsurance)";
+                        string[] tmp = { "ID", "Страна", "Город, место", "Дата начала", "Дата окончания", "Цена", "Туроператор", "Авиакомпания", "Проживание", "Страховка" };
+                        
+                        headerNames = tmp;
+                        tableName = "TourTable";
+                        numOfColumns = 10;                    
+                       
+                        /*GridViewPage1.ColumnCount = 10;
+                        GridViewPage1.Columns[8].Width = 250;    
+                        for (int i = 0; i < GridViewPage1.ColumnCount; ++i)
+                            GridViewPage1.Columns[i].Name = tmp[i];*/                      
+                        break;
+                    }
+
+                case "Проживаниие":
+                    {
+                        TableNameLabel.Text = "Проживание";
+                        query = "SELECT * FROM Accommodation";                      
+                        string[] tmp = { "ID", "Название", "Категория", "Количество звезд", "Питание", "Опции", "Телефон", "Адрес"};
+                        headerNames = tmp;
+                        tableName = "AcTable";
+                        numOfColumns = 8;                        
+                        break;
+                    }
+                case "Авиакомпании":
+                    {
+                        TableNameLabel.Text = "Авиакомпании";
+                        query = "SELECT * FROM Airline Order By IdAirline";                      
+                        string[] tmp = { "ID", "Название", "Класс"};
+                        headerNames = tmp;
+                        tableName = "AirlineTable";
+                        numOfColumns = 3;                       
+                        break;
+                    }
+                case "Страхование":
+                    {
+                        TableNameLabel.Text = "Страхование";
+                        query = "SELECT * FROM Insurance Order By IdInsurance";
+                        string[] tmp = { "ID", "Название компании", "Тип тура", "Категория страхования"};
+                        headerNames = tmp;
+                        tableName = "InsuranceTable";
+                        numOfColumns = 4;                        
+                        break;
+                    }
+                default:
+                    {
+                        MessageBox.Show("Неправильно выбрана таблица!");
+                        return;
+                    }
+            }
+            adap = new SqlDataAdapter(query, conn);
+            data = new System.Data.DataSet();
+            adap.Fill(data, tableName);
+            
+            for (int i = 0; i < numOfColumns; ++i)
+                data.Tables[0].Columns[i].ColumnName = headerNames[i];
+
+            GridViewPage1.DataSource = data.Tables[0];
+            if (conn != null) conn.Close();
+
+            //TableNameLabel.Text = data.Tables[0].Rows[0][0].ToString();
+            //TourGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; - без прокрутки в сторону
+ 
+            //------------------------------вариант 1-------------------------------------------------------------------
+            /*string way = "Data Source=VICKY-PC\\SQLEXPRESS;Initial Catalog=TravelAgency;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(way);
+            conn.Open();*/
+            /*SqlCommand tableContent = new SqlCommand(query, conn);
+            SqlDataReader rdr = tableContent.ExecuteReader();
+
+            string[] tableString = new string[GridViewPage1.ColumnCount];
+            while (rdr.Read())
+            {
+                for (int i = 0; i < GridViewPage1.ColumnCount; ++i)
+                    tableString[i] = rdr[i].ToString();
+                GridViewPage1.Rows.Add(tableString);
+            }
+           
+            if (rdr != null) conn.Close();
+            if (conn != null) conn.Close();*/
+            //---------------------------------------------------------------------------------------------------------
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+   
         }
     }
 }
